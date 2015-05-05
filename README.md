85a3623b0c8e7adb2ddfbe4698a812c19dd0fde2
gilzoide <gilzoide@gmail.com>
2015-05-05 08:03:02 -0300

    Added GPLv3 License; updated README

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 Hell
 ====
 
-Hell is an open source build system written in HaskEL-Lua.
-
-Hell aims to being simple to script, yet very flexible,
-but not letting go of the speed.
+Hell is an open source build system.
 
 It's use is based on _Builders_ for any kind of stuff, that can be created or
 modified on the fly very easily, which brings flexibility and extensibility.
@@ -19,12 +16,6 @@ For building hell, you can use make
 Or, you can use hell itself! (still not able to build, but the hellbuild is there)
 
     $ hell
-
-As build dependencies, we need some Haskell stuff (which can be found and
-installed with cabal):
-- hslua
-- glob
-- io-memoize
 
 Installing
 ----------
--- src/lua/parseOpts.lua
+++ src/lua/parseOpts.lua
@@ -1,6 +1,24 @@
 --- @file parseOpts.lua
 -- Options, right?
 
+--[[
+-- Copyright (C) 2015 Gil Barbosa Reis
+-- This file is part of Hell.
+--
+-- Hell is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Hell is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Hell.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
 local int = require 'internals'
 
 -- all the hell options
